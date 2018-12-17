cf2962a00b95f40cea8da846c32bf632794d6d22
Andy Roxby <aroxby@users.noreply.github.com>
2018-12-16 21:02:46 -0500

    Allow starting % to be given

--- main.py
+++ main.py
@@ -10,35 +10,44 @@ def fix_td_len(tds):
         tds += '.000000'
     return tds
 
-def update(duration, remaining, file, width=100, overwrite=True):
+def update(duration, remaining, file, width=100, overwrite=True, headstart=0):
     width -= 2
+    headstart /= 100.
     tds = fix_td_len('{}'.format(remaining))
-    frac = remaining / duration
-    pro_rem = int(frac * width)
-    pro_com = width - pro_rem
-    perc = int(100 * (1 - frac))
+
+    frac = 1- (remaining / duration)
+    out_of = 1 - headstart
+    frac *= out_of
+    frac += headstart
+    perc = int(100 * frac)
+    pro_com = int(frac * width)
+    pro_rem = width - pro_com
+
     progress = '[' + '=' * pro_com + '-' * pro_rem + ']'
     end = '\r' if overwrite else '\n'
-    
     print_kwargs = {'file': file, 'flush': True, 'end': end}
     print('{} {} ({}%)'.format(tds, progress, perc), **print_kwargs)
 
-def countdown(seconds, file):
+def countdown(seconds, start, file):
     duration = datetime.timedelta(seconds=seconds)
     end = datetime.datetime.now() + duration
     remaining = end - datetime.datetime.now()
     while remaining >= TDZ:
-        update(duration, remaining, file)
+        update(duration, remaining, file, headstart=start)
         sleep(0.1)
         remaining = end - datetime.datetime.now()
     update(duration, TDZ, file, overwrite=False)
 
 def main(argv):
-    if len(argv)!=2:
-        print("Usage: ./main.py seconds", file=sys.stderr)
+    if len(argv) not in [2, 3]:
+        print("Usage: ./main.py seconds [start%]", file=sys.stderr)
     else:
         time = int(argv[1])
-        countdown(time, sys.stdout)
+        if len(argv) > 2:
+            start = int(argv[2])
+        else:
+            start = 0
+        countdown(time, start, sys.stdout)
 
 if __name__ == '__main__':
     main(sys.argv)
